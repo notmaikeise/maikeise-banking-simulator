ebf4725e0356db7c4101d452d670be589c427730
Anny Maikeise <annymaikeise@outlook.com>
2026-09-25 19:05:59 -0300

    chore: initialize Spring Boot application

---
+++ src/test/java/io/github/notmaikeise/bankingsimulator/MaikeiseBankingSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.notmaikeise.bankingsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaikeiseBankingSimulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
